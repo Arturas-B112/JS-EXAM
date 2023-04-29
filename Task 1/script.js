b4abe260da812c060d1f5d61d617e2d666bdf2fe
Arturas-B112 <arturas.borodko@gmail.com>
2023-04-29 07:46:27 +0300

    Removed task 1 from task 2 branch

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,29 +8,3 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 <div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
-
-const weightForm = document.querySelector('form');
-const outpoutDiv = document.getElementById('output');
-
-if (weightForm) {
-  weightForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    outpoutDiv.textContent = '';
-    renderWeights();
-  });
-}
-
-const renderWeights = () => {
-  const kgInput = document.getElementById('search');
-  const userKgValue = kgInput.value;
-  const lbHeading = document.createElement('h3');
-  const gHeading = document.createElement('h3');
-  const ozHeading = document.createElement('h3');
-  const lbWeight = userKgValue * 2.2046;
-  const gWeight = userKgValue / 0.001;
-  const ozWeight = userKgValue * 35.274;
-  lbHeading.textContent = `Your weight in pounds is ${lbWeight.toFixed(2)} lb`;
-  gHeading.textContent = `Your weight in grams is ${gWeight} g`;
-  ozHeading.textContent = `Your weight in ounces is ${ozWeight.toFixed(2)} oz`;
-  outpoutDiv.append(lbHeading, gHeading, ozHeading);
-};
